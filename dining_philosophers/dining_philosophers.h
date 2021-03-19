d1b94fad7017e0c32c6104c2b20c85de7b0e3fd7
Fellipe Bravo <fellipe.bravo@gmail.com>
2021-03-19 10:25:11 -0300

    Added dining philosophers header

--- dining_philosophers/dining_philosophers.h
+++ dining_philosophers/dining_philosophers.h
@@ -1,6 +1,38 @@
 #ifndef DINING_PHILOSOPHERS
 #define DINING_PHILOSOPHERS
 
+/**
+ * The Fork struct
+ * Contains an int value that indicates if the resource is taken.
+ **/
+typedef struct
+{
+	int isTaken;
+} Fork;
 
+/**
+ * The Philosopher struct
+ * Contains two pointers that simulate hands pointing to Forks.
+ **/
+typedef struct
+{
+	Fork *leftHand;
+	Fork *rightHand;
+} Philosopher;
+
+/**
+ * Vinculates a Fork to a Philosopher
+ **/
+void takeFork(Philosopher *philosopher, Fork *fork);
+
+/**
+ * The Philosopher starts eating
+ **/
+void eat(Philosopher *philosopher);
+
+/**
+ * The Philosopher starts thinking
+ **/
+void think(Philosopher *philosopher);
 
 #endif
